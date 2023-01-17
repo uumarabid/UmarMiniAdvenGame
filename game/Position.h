dde08c07514851ae4042dba334d4da2e3dcb6441
Umar1311829 <star_umar2008@hotmail.com>
2023-01-17 13:54:56 +0000

    Testing grid movement for user

--- game/Position.h
+++ game/Position.h
@@ -2,14 +2,16 @@
 #include <iostream>
 #include<string>
 #include "Color.h"
+#include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 // room class
 class Position {
-//private:
-//	string description;
-//	bool dragon;
+	//private:
+	//	string description;
+	//	bool dragon;
 public:
 	int locationGrid[8][8];
 	int x = 0;
@@ -29,26 +31,40 @@ Position::Position() {
 
 }
 
-void Position::moveUp() {
+void Position::moveLeft() {
 	if (y > 0)
 		y--;
 	userPosition();
 }
-void Position::moveDown() {
+void Position::moveRight() {
 	if (y < 5)
 		y++;
 	userPosition();
 }
-void Position::moveLeft() {
+void Position::moveUp() {
 	if (x > 0)
 		x--;
 	userPosition();
 }
-void Position::moveRight() {
+void Position::moveDown() {
 	if (x < 5)
 		x++;
 	userPosition();
 }
 void Position::userPosition() {
-	cout << "[" << x << "][" << y << "]";
+	int localx = x, localy = y; // position of the *
+	char grid[8][8];
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			grid[i][j] = '.';
+		}
+	}
+	grid[localx][localy] = '*';
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			std::cout << grid[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+	cout << "\n\n";
 }
